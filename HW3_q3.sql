645072ed713f2ebc08e9b077e8b0f78a2b247db6
CloudD233 <92987790+CloudD233@users.noreply.github.com>
2024-03-12 14:17:38 -0400

    Create HW3_q3.sql

---
+++ HW3_q3.sql
@@ -0,0 +1,45 @@
+CREATE OR REPLACE PROCEDURE displayArtist (
+    varFirstName char,
+    varLastName char 
+)
+AS
+
+    readArtistID NUMBER; 
+    readFirstName char(25); 
+    readLastName char(25); 
+    readnationality char(30); 
+    readDOB NUMBER; 
+    readDD NUMBER; 
+    readTitle char(35); 
+    readMedium char(35); 
+    readdescription VARCHAR2; 
+    
+    CURSOR artistcursor IS 
+        SELECT ArtistID, FirstName, LastName, Nationality, DateofBirth, DateOfBirth, DateDeceased
+        FROM ARTIST 
+        WHERE LastName = varLastName AND FirstName = varFirstName;
+        
+    CURSOR workcursor IS 
+        SELECT Title, Medium, description
+        FROM WORK 
+        WHERE artist.ArtistID = work.ArtistID; 
+        
+    
+BEGIN 
+
+    FOR artist IN artistcursor
+        LOOP 
+        readLastName := artist.LastName; 
+        readFirstName := artist.FirstName; 
+        readNationality := artist.Nationality; 
+        readDOB := artist.DateOfBirth; 
+        readDD := artist.DateDeceased; 
+        readArtistID := artist.ArtistID; 
+        readTitle := workcursor.Title; 
+        readMedium := workcursor.Medium;
+        readdescription := workcursor.description; 
+        END LOOP; 
+        
+END; 
+/
+    
