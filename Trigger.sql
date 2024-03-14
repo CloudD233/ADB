34a383b6aa0db00dfcc63af6b8df39734da35d0c
CloudD233 <92987790+CloudD233@users.noreply.github.com>
2024-03-14 14:33:52 -0400

    Update Trigger.sql

--- Trigger.sql
+++ Trigger.sql
@@ -1,32 +1,40 @@
 --SQL-PSM Worksheet 3
-
 --QUESTION 4
-CREATE OR REPLACE TRIGGER DisallowWorkID 
-BEFORE UPDATE OF WORKID ON WORK
-    
-BEGIN 
-    raise_application_error(-20000, 'Warning: NO CANT DO');
-END;
-/
+CREATE
+OR REPLACE TRIGGER DisallowWorkID BEFORE
+UPDATE
+    OF WORKID ON WORK BEGIN raise_application_error(-20000, 'Warning: NO CANT DO');
 
---QUESTION 5 
-update WORK set WORKID = 1;
+END;
+
+/ --QUESTION 5 
+update
+    WORK
+set
+    WORKID = 1;
 
 --QUESTION 6
 --make table and insert
 CREATE TABLE ALLOWED_NATIONALITY (Nation Char(30) PRIMARY KEY);
 
-INSERT INTO ALLOWED_NATIONALITY (Nation)
-VALUES('United States');
-ALTER TABLE artist DROP CONSTRAINT nationalityvalues;
+INSERT INTO
+    ALLOWED_NATIONALITY (Nation)
+VALUES
+('United States');
+
+ALTER TABLE
+    artist DROP CONSTRAINT nationalityvalues;
 
 --Trigger
-    CREATE OR REPLACE TRIGGER preventUpdate
-    BEFORE UPDATE OF Nation ON ALLOWED_NATIONALITY
-    
-    BEGIN 
-        IF NEW.Nation IS NOT DISTINCT FROM OLD.Nation THEN
-        raise_application_error(-20000, 'Warning: NO CANT DO');
-        END IF; 
-        END;
-        /
+CREATE
+OR REPLACE TRIGGER preventUpdate BEFORE
+UPDATE
+    OF Nation ON ALLOWED_NATIONALITY BEGIN IF NEW.Nation IS NOT DISTINCT
+FROM
+    OLD.Nation THEN raise_application_error(-20000, 'Warning: NO CANT DO');
+
+END IF;
+
+END;
+
+/
