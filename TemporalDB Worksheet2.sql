13d7bea8b12a2106b1866d21cb253bc81b9391a1
CloudD233 <92987790+CloudD233@users.noreply.github.com>
2024-04-11 17:07:16 -0400

    Update TemporalDB Worksheet2.sql

--- TemporalDB Worksheet2.sql
+++ TemporalDB Worksheet2.sql
@@ -4,3 +4,16 @@ eid NUMBer,
 name VARCHAR(10),
 salary NUMBER,
 CONSTRAINT empTT_pk PRIMARY KEY(eid));
+
+--4 
+INSERT INTO empTT(eid, name, salary)
+VALUES(1, 'Cloud', 100);
+
+INSERT INTO empTT(eid, name, salary)
+VALUES(2, 'Chris', 150);
+
+--5 
+select current_scn from v$database;
+--The number: 529513182
+
+--6 
