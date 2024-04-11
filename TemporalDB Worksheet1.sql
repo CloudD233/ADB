36c5f5ec1d71844ceac6261d513ae63b8bba8d3b
CloudD233 <92987790+CloudD233@users.noreply.github.com>
2024-04-11 15:25:17 -0400

    Update TemporalDB Worksheet1.sql

--- TemporalDB Worksheet1.sql
+++ TemporalDB Worksheet1.sql
@@ -29,7 +29,33 @@ WHERE projNo = 1001 AND empId = 103;
 
 --12 
 UPDATE Assign
-SET PROJNO = 1030, valid_time_start = TO_DATE('31-dec-2022')
+SET PROJNO = 1030, valid_time_start = TO_DATE('31-dec-2022'), hoursAssigned = 15
 WHERE projNo = 1001 AND empId = 103;
+
+--13 
+SELECT projNo, empId, valid_time_end
+FROM Assign
+WHERE valid_time_end IS NULL OR valid_time_end > CURRENT_DATE;
+
+--14 
+SELECT projNo, empId, valid_time_end
+FROM Assign
+WHERE TO_DATE('02-Aug-1964') BETWEEN valid_time_start AND valid_time_end
+OR (valid_time_start <= TO_DATE('02-Aug-1964') AND valid_time_end IS NULL);
+
+--15
+SELECT projNo, empId, valid_time_end
+FROM Assign
+WHERE TO_DATE('04-Jul-2002') BETWEEN valid_time_start AND valid_time_end
+OR (valid_time_start <= TO_DATE('04-Jul-2002') AND valid_time_end IS NULL);
+
+--16
+SELECT projNo, empId, valid_time_start, valid_time_end
+FROM Assign
+WHERE (valid_time_start <= TO_DATE('03-Dec-2022') 
+AND (valid_time_end >= TO_DATE('03-Dec-2022') 
+OR valid_time_end IS NULL));
+
+
 
 
